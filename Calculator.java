cf4e3926168e18fa4162297638cf3fe6836f6441
Carlos Olvera <carlosolvera@mac.lan>
2025-06-27 21:08:45 -0400

    Inital run good

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,28 @@
-public class Calculator {
+public class Calculator{
+    private int operand1;
+
+    public Calculator(int op1){
+        this.operand1 = op1;
+    }
+    public Calculator(){
+
+    }
+
+    public int add(Calculator op2){
+        return operand1 + op2.operand1;
+    }
+
+    public int subtract(Calculator op2){
+        return operand1 - op2.operand1;
+    }
+
+    public int divide(Calculator op2){
+        return operand1 / op2.operand1;
+    }
+
+    public int multiply(Calculator op2){
+        return operand1 * op2.operand1;
+    }
+        
     
 }
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main extends Calculator {
+    public static void main(String[] args){
+        Calculator x = new Calculator(1);
+        Calculator y = new Calculator(2);
+        System.out.println(x.add(y));
+
+    }
+}
