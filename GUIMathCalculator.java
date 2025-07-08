760c33985e18fdb70f440cfc1edfbc475c307dab
Carlos Olvera <carlosolvera@Carloss-Laptop.local>
2025-07-08 02:06:21 -0400

    GUI something is wrong, text not appearing

--- GUIMathCalculator.java
+++ GUIMathCalculator.java
@@ -1,30 +1,44 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;  // Added this
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class GUIMathCalculator {
+public class GUIMathCalculator implements ActionListener{
     // Moved main method inside class
-    public static void main(String[] args){
-        new GUIMathCalculator();
-    }
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
     
     public GUIMathCalculator(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        frame = new JFrame();
+        panel = new JPanel();
         JButton button1 = new JButton("1");
-        button1.
+        button1.addActionListener(this);
+        label = new JLabel("Number of clicks: 0");
         panel.setBorder(BorderFactory.createEmptyBorder(500, 500, 10, 10));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(button1);
-
-
+        
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("CalculatorGUI");
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new GUIMathCalculator();
+    }
+
+    public void actionPerformed(ActionEvent e){
+        count++;
+        label.setText("Pressed" + count +  "times");
     }
 }
